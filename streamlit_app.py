aaf8e515099ced05ed30e0c9ddc164e13d4b24b2
Apichaya Thaneerat <97445436+Plagrim-Apichaya@users.noreply.github.com>
2023-10-13 22:00:56 -0400

    rr

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,17 @@
-import pandas as pd
-import seaborn as sns
 import streamlit as st
-import altair as alt
-from altair import datum
-import math
-import numpy as np
+import time
+import pandas as pd
 
-option = st.selectbox("1","2","3")
+col1, col2, col3 = st.columns([1, 2, 1])
 
-st.write('data selected:', option)
+col1.markdown(" # Welcome to my app! ")
+col1.markdown(" # Here is some info on the app. ")
+col2.markdown(" Thailand Food Price (2014 - 2019) ")
 
-url = "https://raw.githubusercontent.com/Plagrim-Apichaya/830_f23_midterm/main/thailand-food-median-prices-2.csv"
+#upload_photo = col2.file_uploader(" Upload a photo", on_change = "chage_photo_state")
+input_photo = col2.camera_input(" Take a photo to record your log in today")
+
+url = "https://raw.githubusercontent.com/Plagrim-Apichaya/CMSE830_23/main/thailand-food-median-prices-2.csv?token=GHSAT0AAAAAACH3C2B3L45MDEEGQ7VW6TX4ZIXTBUQ"
 th_food = pd.read_csv(url)
 st.write(" ## dataset")
 st.dataframe(th_food)
