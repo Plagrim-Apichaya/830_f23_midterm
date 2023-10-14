507d9af4ac8e73e1c2c6c20548a99717968b8486
Apichaya Thaneerat <97445436+Plagrim-Apichaya@users.noreply.github.com>
2023-10-13 22:32:51 -0400

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title ("this is the app title")
+st.title("this is the app title")
 st.header("this is the markdown")
 st.markdown("this is the header")
 st.subheader("this is the subheader")
