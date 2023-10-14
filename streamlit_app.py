4a7f1850e3cf868486a446529382bf6d8440ae3f
Apichaya Thaneerat <97445436+Plagrim-Apichaya@users.noreply.github.com>
2023-10-13 21:30:54 -0400

    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.write(" # Analysis of Thai agriculture price and the driven factor behind the price dynamic.")
+# Set the title of the Streamlit app
+st.title("My Streamlit App")
+
+# Create a header
+st.header("This is a Streamlit App with a Heading")
+#st.write(" # Analysis of Thai agriculture price and the driven factor behind the price dynamic.")
 
 url = "https://raw.githubusercontent.com/Plagrim-Apichaya/830_f23_midterm/main/thailand-food-median-prices-2.csv"
 th_food = pd.read_csv(url)
