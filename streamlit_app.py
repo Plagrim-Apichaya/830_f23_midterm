30f1323a4e55fec6837205a549e79fe83b435911
Apichaya Thaneerat <97445436+Plagrim-Apichaya@users.noreply.github.com>
2023-10-19 01:25:55 -0400

    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ if page == "Home":
     st.subheader("This app is to walk you through the analyze of the historical agriculture price data recorded monthly from January 2001 to March 2020")
     st.write("Thailand is a dynamic developing country which is known for its significant contributions to global agriculture exporting. It is a major exporter of agricultural products, with a reputation for delivering their culture along with them  like rice, fruits, cassava, and fish. These agricultural exports not only deliver and serve the nation's economy but also serve as Thai culture and traditions. Additionally, they fulfill the dietary needs of the Thai population. However, the increasing prices of these fundamentals have raised concerns in understanding the intricate dynamics governing their fluctuations.")  
 
-    a = st.multiselect("Agriculture product type", ["Rice", "Corn", "Cassava"])
-    st.write(a)
+    veg_type = st.multiselect("Agriculture product type", ["Rice", "Corn", "Cassava"])
+    
 
     #url = "https://raw.githubusercontent.com/Plagrim-Apichaya/830_f23_midterm/main/thailand-food-median-prices-2.csv"
-    url = "https://raw.githubusercontent.com/Plagrim-Apichaya/830_f23_midterm/main/Thailand_food_price.csv"
+    url = "https://raw.githubusercontent.com/Plagrim-Apichaya/830_f23_midterm/main/TH_agri_price.csv"
     df = pd.read_csv(url, index_col=0)
     st.dataframe(df)
 
